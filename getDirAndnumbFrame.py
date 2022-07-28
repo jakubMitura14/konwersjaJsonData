ebe6f2e6238638e3aae9c0a7d21ad92e99a67d57
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-28 10:21:29 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- getDirAndnumbFrame.py
+++ getDirAndnumbFrame.py
@@ -18,51 +18,69 @@ from os import path as pathOs
 from pathlib import Path
 
 orig_data_dir='/workspaces/konwersjaJsonData/nas-lssi-dco'
-
+resCSVDir='/workspaces/orig_files_dir.csv'
 #getting paths to dicom dir files
-listOfPatsh=mainFuncs.get_all_file_paths(orig_data_dir)
-pathsDICOMDIR= list(filter(lambda pathh:  'DICOMDIR' in pathh,listOfPatsh ))
 
 
-currentDicomDirPath=pathsDICOMDIR[0]
-pathhh = Path(currentDicomDirPath)
-currentFold=str(os.path.relpath(pathhh.parent))
 
-ds = pydicom.dcmread(currentDicomDirPath)
-fs = FileSet(ds)
-listInfs = list(fs)
-current_file_ref = listInfs[0]
 
-def getFileData(current_file,currentFold)
+def getListMeta(currentDicomDirPath):
+    pathhh = Path(currentDicomDirPath)
+    currentFold=str(os.path.relpath(pathhh.parent))
+    ds = pydicom.dcmread(currentDicomDirPath)
+    fs = FileSet(ds)
+    fileMataDatas=list(map(partial(getFileData,currentFold=currentFold),list(fs)))
+    return fileMataDatas
+
+def getFileData(current_file_ref,currentFold):
+    filePath=current_file_ref.path
     current_file = current_file_ref.load()
     sop=mainFuncs.get_SOPInstanceUID(current_file)
     SeriesInstanceUID= mainFuncs.get_SeriesInstanceUID(current_file)
     stidy_instanceUID=mainFuncs.get_StudyInstanceUID(current_file)
-    return (currentFold,stidy_instanceUID,SeriesInstanceUID,sop  )
+    return (currentFold,filePath,stidy_instanceUID,SeriesInstanceUID,sop  )
 
 
-
-
-exampleDicomDir= '/workspaces/konwersjaJsonData/nas-lssi-dco/829/DICOMDIR'
-ds = pydicom.dcmread(exampleDicomDir)
-fs = FileSet(ds)
-
-for instance in fs:
-    print(instance.load().file_meta)
-    
-
-def get_df_orig_dir_info(dataDir):
-    listOfPatsh=get_all_file_paths(dataDir)
+def get_df_orig_dir_info(orig_data_dir):
+    #get all paths of DICOMDIR files
+    listOfPatsh=mainFuncs.get_all_file_paths(orig_data_dir)
+    pathsDICOMDIR= list(filter(lambda pathh:  'DICOMDIR' in pathh,listOfPatsh ))
     df = pd.DataFrame()
-    df['paths']=listOfPatsh
     resList=[]
     with mp.Pool(processes = mp.cpu_count()) as pool:
-        resList=pool.map(getUids  ,list(df.iterrows()))
+        resList=pool.map(getListMeta ,pathsDICOMDIR)
+    #flatten array
+    resList=list(itertools.chain(*resList))
 
-    sops = list(map(lambda tupl: tupl[0], resList))
-    SeriesInstanceUIDs = list(map(lambda tupl: tupl[1], resList))
-    InstanceUIDs = list(map(lambda tupl: tupl[2], resList))
+# (currentFold,filePath,stidy_instanceUID,SeriesInstanceUID,sop  )
+    masterolds=list(map(lambda tupl: tupl[0], resList))
+    currentFilePath=list(map(lambda tupl: tupl[1], resList))
+    StudyInstanceUID = list(map(lambda tupl: tupl[2], resList))
+    SeriesInstanceUIDs = list(map(lambda tupl: tupl[3], resList))
+    sops = list(map(lambda tupl: tupl[4], resList))
+
+    df['masterolds']=masterolds   
+    df['currentFilePath']=currentFilePath   
     df['SOPInstanceUID']=sops   
     df['SeriesInstanceUID']=SeriesInstanceUIDs   
-    df['StudyInstanceUID']=InstanceUIDs   
+    df['StudyInstanceUID']=StudyInstanceUID   
     return df 
+
+
+resDf=get_df_orig_dir_info(orig_data_dir)
+resDf.to_csv(resCSVDir)
+
+print(resDf)
+
+# currentDicomDirPath=pathsDICOMDIR[0]
+
+# allMetaDat=[]
+# with mp.Pool(processes = mp.cpu_count()) as pool:
+#     allMetaDat=pool.map( getListMeta ,pathsDICOMDIR )
+
+# allMetaDat_flat=list(itertools.chain(*allMetaDat))
+
+# print(allMetaDat_flat)
+
+
+    
