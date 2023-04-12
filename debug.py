34c3ed5a17e12608e8a1063000a5254f282f9e9f
Jakub Mitura <jakub.mitura14@gmail.com>
2023-04-12 07:33:50 +0000

    up

---
+++ debug.py
@@ -0,0 +1,52 @@
+
+import SimpleITK as sitk
+import mdai
+import pandas as pd
+import numpy as np
+import cv2
+import pydicom
+import os
+import multiprocessing as mp
+import functools
+from functools import partial
+import mdai
+import math
+import time
+import mainFuncs
+import itertools
+from pydicom.fileset import FileSet
+from os import path as pathOs
+from pathlib import Path
+import toolz
+from toolz.curried import pipe, map, filter, get
+from toolz import curry
+import getDirAndnumbFrame
+from getDirAndnumbFrame import get_df_orig_dir_info
+import get3dFiles
+from get3dFiles import get_frame_with_output
+import preprocess
+from preprocess import dilatate_erode_conditionally
+from os.path import basename, dirname, exists, isdir, join, split
+import nnunetv2
+
+import elastixRegister
+from elastixRegister import reg_a_to_b
+import tempfile
+import shutil
+import re
+from toolz.itertoolz import groupby
+from toolz import curry
+# import multiprocess
+# p = multiprocess.Pool(os.cpu_count())
+import multiprocessing as mp
+import json
+import SimpleITK as sitk
+
+
+
+file_path='/home/sliceruser/workspaces/konwersjaJsonData/nnunetMainFolder/nnUNet_raw/Dataset279_Prostate/imagesTr/9004900_0000.nii.gz'
+file_path_b='/workspaces/konwersjaJsonData/explore/9004900_0000.nii.gz'
+image = sitk.ReadImage(file_path)
+print(image)
+# cp /home/sliceruser/workspaces/konwersjaJsonData/nnunetMainFolder/nnUNet_raw/Dataset279_Prostate/imagesTr/9004900_0000.nii.gz /home/sliceruser/workspaces/konwersjaJsonData/explore/9004900_0000.nii.gz
+
