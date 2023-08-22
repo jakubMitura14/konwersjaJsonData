01f9c964bd0107a2d81dc7b8dbfcd812ec244196
Jakub Mitura <jakub.mitura14@gmail.com>
2023-08-22 09:35:00 +0000

    up

--- nnunet/nnunetv2pl/nnUNet/nnunetv2/training/nnUNetTrainer/Pl_model.py
+++ nnunet/nnunetv2pl/nnUNet/nnunetv2/training/nnUNetTrainer/Pl_model.py
@@ -124,7 +124,7 @@ class Pl_Model(pl.LightningModule):
     def configure_optimizers(self):
         # optimizer = torch.optim.SGD(self.network.parameters(), self.learning_rate, weight_decay=self.weight_decay,
         #                             momentum=0.99, nesterov=True)
-        optimizer = torch.optim.AdamW(self.network.parameters(), self.learning_rate)
+        optimizer = torch.optim.AdamW(self.network.parameters(), 0.003311311214825908)#learning rate set by learning rate finder
         
         # hyperparameters from https://www.kaggle.com/code/isbhargav/guide-to-pytorch-learning-rate-scheduling/notebook
         lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=10, T_mult=1, eta_min=0.001, last_epoch=-1 )
@@ -155,7 +155,6 @@ class Pl_Model(pl.LightningModule):
         data = batch['data']
         target = batch['target']
         output = self.network(data)
-        print(f"trainnn data 0 {data[0].shape} {data[1].shape} {data[2].shape}")
 
         epoch=self.current_epoch
         l=self.loss(output, target)
@@ -200,10 +199,10 @@ class Pl_Model(pl.LightningModule):
 
         data = batch['data']
         target = batch['target']
-        if(isinstance(data,list)):
-            print(f"valll data 0 {data[0].shape} {data[1].shape} {data[2].shape}")
-        else:
-            print(f"valll data 0 {data.shape}")
+        # if(isinstance(data,list)):
+        #     print(f"valll data 0 {data[0].shape} {data[1].shape} {data[2].shape}")
+        # else:
+        #     print(f"valll data 0 {data.shape}")
 
         
         epoch=self.current_epoch
