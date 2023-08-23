cd7f7a2d6f5a31f02c4dc673fd73b89da148dbe8
Jakub Mitura <jakub.mitura14@gmail.com>
2023-08-23 17:28:59 +0000

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- nnunet/for_pre_training.py
+++ nnunet/for_pre_training.py
@@ -2,3 +2,4 @@ https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=70226903
 https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=70225642
 https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=119705830
 https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=50135447
+
