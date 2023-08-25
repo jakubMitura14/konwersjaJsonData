f74d6c8c740b3bd5f26b134cca8a01add3f814ae
Jakub Mitura <jakub.mitura14@gmail.com>
2023-08-25 14:12:10 +0000

    up

--- nnunet/nnunetv2pl/nnUNet/nnunetv2/training/nnUNetTrainer/My_pl_trainer.py
+++ nnunet/nnunetv2pl/nnUNet/nnunetv2/training/nnUNetTrainer/My_pl_trainer.py
@@ -189,7 +189,7 @@ class My_pl_trainer(nnUNetTrainer):
 
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.network.parameters(), self.initial_lr, weight_decay=self.weight_decay,
+        optimizer = SAM(torch.optim.SGD,self.network.parameters(), lr=self.initial_lr, weight_decay=self.weight_decay,
                                     momentum=0.99, nesterov=True)
         lr_scheduler = CosineAnnealingLR(optimizer, T_max=self.num_epochs)
         return optimizer, lr_scheduler
