278fc020179f94572e14a293a9d8e285eb48a9de
Jakub Mitura <jakub.mitura14@gmail.com>
2023-11-18 13:32:13 +0000

    up

--- hyperopt/hyperparam_tuning.py
+++ hyperopt/hyperparam_tuning.py
@@ -210,7 +210,7 @@ def set_norm_and_bias_field(trial):
 # experiment_name="general_augment"
 # experiment_name="classic_augmentations2"#bias_norm
 # experiment_name="test"#bias_norm
-experiment_name="classic_augmentations11"#bias_norm
+experiment_name="classic_augmentations12"#bias_norm
 
 
 def setup_pseudo_lesion_adder_and_loss(trial):
